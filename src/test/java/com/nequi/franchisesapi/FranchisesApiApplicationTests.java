e50a6c8e214364896cf95f10d1ba56a35f93a547
Esteban Gutierrez <esteban.gutierrez@ESTEBAN-GUTIERREZ.local>
2025-03-15 10:44:26 -0500

    init franchise api

---
+++ src/test/java/com/nequi/franchisesapi/FranchisesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nequi.franchisesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchisesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
